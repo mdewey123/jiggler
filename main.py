1b1fb5689624ad905e271e27ffa07d6c8862946a
mdewey123 <147895420+mdewey123@users.noreply.github.com>
2024-12-13 13:31:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ def get_cordinates():
     width = [0]
     height = [1]
     
-    return(display, width, height)
+    return(width, height)
     
 def wiggle_mouse():
-    pyautogui.moveTo(x, y, duration=num_seconds)
+    get_cordinates()
+    x_dest = random.randint(10, int(width)-10)
+    y_dest = random.randint(10, int(height)-10)
+    pyautogui.moveTo(x_dest, y_dest, duration=6.9)
 
 def shutdown()
