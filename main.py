69155da32b83aab2e4a7c7bd49f54ca9a1a5d366
mdewey123 <147895420+mdewey123@users.noreply.github.com>
2024-12-13 16:12:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,59 @@
-import pyautogui, time, random, sys
+import pyautogui, threading, random, sys, keyboard
 
 pyautogui.FAILSAFE = True
+pyautogui.PAUSE = 3
 
-def get_cordinates():
+
+def get_coordinates():
     display = pyautogui.size()
-    width = [0]
-    height = [1]
-    
-    return(width, height)
+    width = display[0]
+    height = display[1]
+    return width, height
     
 def wiggle_mouse():
-    get_cordinates()
-    x_dest = random.randint(10, int(width)-10)
-    y_dest = random.randint(10, int(height)-10)
-    pyautogui.moveTo(x_dest, y_dest, duration=6.9)
+    width, height = get_coordinates()
+    
+    for i in range(random.randint(1, 6)):
+        x_dest = random.randint(10, width-10)
+        y_dest = random.randint(10, height-10)
+        pyautogui.moveTo(x_dest, y_dest, duration=6.9)
+    
+    shutdown()
+    
+    
+def shutdown():
+    def timeout():
+        nonlocal result
+        result = 'Continue'
+    
+    result = None
+    timer = threading.Timer(30, timeout)
+    timer.start()
+    
+    try:
+        result = pyautogui.confirm(
+            text='Do you want to cancel the wiggler?', 
+            title='Finished?', 
+            buttons=['Finish', 'Continue']
+        )
+    
+    finally:  
+        timer.cancel()
+        
+    if result == "Finish":
+        print("Wiggler is going to take a break now")
+        sys.exit()
+    elif result == 'Continue':
+        print("Wiggler is doing his best!")
+        wiggle_mouse()
+    
+def listen_4p():
+    while True:
+        if keyboard.is_pressed('shift+p'):
+            print('Wiggler is going to take a break now.')
+            sys.exit()
 
-def shutdown()
+listener_thread = threading.Thread(target=listen_4p, daemon=True)
+listener_thread.start()
+    
+wiggle_mouse()
